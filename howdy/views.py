d0113825b15760f5b22d0077989c4325163cb887
Juan Orduz <sebastianorduz97@gmail.com>
2020-02-05 14:53:37 -0500

    Developer Onboarding four card

---
+++ howdy/views.py
@@ -0,0 +1,12 @@
+# howdy/views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "index.html"
+    # def get(self, request, **kwargs):
+    #     return render(request, 'index.html', context=None)
+
+# Add this view
+class AboutPageView(TemplateView):
+    template_name = "about.html"
